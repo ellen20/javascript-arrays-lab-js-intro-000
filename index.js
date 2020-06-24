fa84ccb713e43f16469e745d615160a1d551678a
jingling jin <jljin0524@gmail.com>
2020-06-24 18:33:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,5 +24,6 @@ function prependKitten(name){
   return [name,...kittens]
 }
 function removeLastKitten(){
-  return kittens.slice(0,kittens.lenght-1)
+  array = kittens.slice(0,kittens.lenght-1)
+  return array
 }
